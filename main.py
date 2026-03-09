512ad4a3ea39bdd391b4c50931e90c5b15ec6e00
Jose-langL <totherrerajoseluis>
2026-03-09 08:27:17 -0600

    feat: :sparkles: Add menu and  fuction

---
+++ main.py
@@ -0,0 +1,21 @@
+def menu_principal(): 
+    menu = """ 
+    1. Agregar producto 
+    2.  Listar producto 
+    3.  Salir
+    """
+    print(menu) 
+    
+while True: 
+    menu_principal()
+    opc = int(input("Ingrese alguna de las opciones: "))
+    
+    if opc == 3:
+        print("BYE bye ")
+        break
+    elif opc == 1: 
+        print("Hola")
+    elif opc == 2: 
+        print("Bro")
+    else: 
+        print("opcion invalida")
